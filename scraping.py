2b8a38cced30506b2d6c1142bcdb34037185074f
bonnie shen <boyueshen@gmail.com>
2018-11-09 11:28:14 -0800

    put sleep time for scraping

--- scraping.py
+++ scraping.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 import requests
-import requests
 import json
 import string
 from urllib.parse import urlparse
 import re
+import time 
 
 def get_all_urls():
 #   Get all the deal links from deal list page
@@ -21,22 +21,22 @@ def get_all_urls():
             double_url.append(x['text']['raw'])
     table = str.maketrans(dict.fromkeys("()"))
     urls = []
-    for s in enumerate(double_url):
+    for i,s in enumerate(double_url):
         if ']' in s:
             urls.append(s.split(']')[1].translate(table))
         else:
             urls.append(s)
-    return (urls)
+    return urls
 
 def feature_scrapping(url):
     features = {}
-    soup = BeautifulSoup(resp = requests.get(url).content,'html.parser')
-    time = soup.find('time')['datetime']
+    soup = BeautifulSoup(requests.get(url).content,'html.parser')
     item_features = soup.find(class_='features').find('ul').text.strip()
     spec_url = soup.find(class_='specs')['href']
-    spe_soup = BeautifulSoup(requests.get(spec_url).content,'html.parser')
+    spe_soup =BeautifulSoup(requests.get(spec_url).content,'html.parser')
     item = spe_soup.find(class_='p-name').text
     item_id = spe_soup.find(class_='h-entry topic unread')['id']
+    date = spe_soup.find('time')['datetime']
     condition = re.compile(r'Condition')
     specs = spe_soup.find('li', class_='comment')
     condition = specs.find(text=condition)
@@ -56,7 +56,7 @@ def feature_scrapping(url):
     sale_num = int(soup.find(id='sold-quantity').text)
     sale_revenue = int(soup.find(id = 'sold-revenue').text.strip('$'))
     poll_num = int(soup.find(class_='vote-count').text.split()[0])
-    features['time']=time
+    features['datetime']=date
     features['item_id']=item_id
     features['item_name']=item
     features['item_features']=item_features
@@ -70,5 +70,19 @@ def feature_scrapping(url):
     features['sale_num']=sale_num
     features['sale_revenue']=sale_revenue
     features['poll_num']=poll_num
-    return features, ref_dict
+    return {**features, **ref_dict}
+
+
+if __name__=='__main__':
+    urls = get_all_urls()[1:]
+    rows = []
+    for i, url in enumerate(urls):
+        print (i)
+        columns.append(feature_scrapping(url))
+        time.sleep(np.random.randint(3,10))
+    data = pd.DataFrame(rows)
+    data.to_csv('meh_data',sep='\t')
+        
+        
+        
     
